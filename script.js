379f56cb1e908e46974bc7fa34431199a52fe5c2
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-04-27 01:00:47 +0200

    Add UserAvatar component

--- script.js
+++ script.js
@@ -37,15 +37,21 @@ function ContactsList() {
 }
 
 function ContactItem({ login, name, department }) {
-  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
   return (
     <li className="item">
-      <img src={imgUrl} className="ui mini rounded image" />
+      <UserAvatar login={login}/>
       <div className="content">
         <h4 className="header">{name}</h4>
         <div className="description">{department}</div>
       </div>
     </li>
+  );
+}
+
+function UserAvatar({ login }) {
+  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+  return (
+    <img src={imgUrl} className="ui mini rounded image" />
   );
 }
 
