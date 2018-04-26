358c4bd5036b1ea058550beda9e17e9a5be37d27
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-04-27 00:49:01 +0200

    Add more components

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-function MyComponent({ title="Modal", content }) {
+function AppHeader() {
+  return (
+    <header className="ui fixed menu">
+      <nav className="ui container">
+        <a href="#" className="header item">
+          <img className="logo" src="http://placehold.it/35x35/00ffff/000000?text=oo" />
+          Lista kontaktów
+        </a>
+        <div className="header item">
+          <button className="ui button">Dodaj</button>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+function ContactsList() {
+  return (
+    <ul className="ui relaxed divided list selection">
+      <ContactItem
+        login="typeofweb1"
+        name="Lena"
+        department="JavaScript Developer"
+      />
+      <ContactItem
+        login="typeofweb2"
+        name="Brian"
+        department="Human Resources"
+      />
+      <ContactItem
+        login="typeofweb3"
+        name="Rick"
+        department="QA"
+      />
+    </ul>
+  );
+}
+
+function ContactItem({ login, name, department }) {
+  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+  return (
+    <li className="item">
+      <img src={imgUrl} className="ui mini rounded image" />
+      <div className="content">
+        <h4 className="header">{name}</h4>
+        <div className="description">{department}</div>
+      </div>
+    </li>
+  );
+}
+
+function App() {
   return (
     <div>
-      <dialog open>
-        <h1>{title}</h1>
-        <p>{content}</p>
-      </dialog>
+      <AppHeader />
+      <main className="ui main text container">
+        <ContactsList />
+      </main>
     </div>
   );
 }
 
-ReactDOM.render(
-  <MyComponent content="A tu jest treść" />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
