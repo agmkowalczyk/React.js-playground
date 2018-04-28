57dd28ba936919625c3d3666bbc2f909e698cf81
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-04-28 23:20:41 +0200

    Add classes

--- script.js
+++ script.js
@@ -1,69 +1,81 @@
-function AppHeader() {
-  return (
-    <header className="ui fixed menu">
-      <nav className="ui container">
-        <a href="#" className="header item">
-          <img className="logo" src="http://placehold.it/35x35/00ffff/000000?text=oo" />
-          Lista kontaktów
-        </a>
-        <div className="header item">
-          <button className="ui button">Dodaj</button>
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <header className="ui fixed menu">
+        <nav className="ui container">
+          <a href="#" className="header item">
+            <img className="logo" src="http://placehold.it/35x35/00ffff/000000?text=oo" />
+            Lista kontaktów
+          </a>
+          <div className="header item">
+            <button className="ui button">Dodaj</button>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+class ContactsList extends React.Component {
+  render() {
+    return (
+      <ul className="ui relaxed divided list selection">
+        <ContactItem
+          login="typeofweb1"
+          name="Lena"
+          department="JavaScript Developer"
+        />
+        <ContactItem
+          login="typeofweb2"
+          name="Brian"
+          department="Human Resources"
+        />
+        <ContactItem
+          login="typeofweb3"
+          name="Rick"
+          department="QA"
+        />
+      </ul>
+    );
+  }
+}
+
+class ContactItem extends React.Component {
+  render() {
+    const { login, name, department } = this.props;
+    return (
+      <li className="item">
+        <UserAvatar login={login}/>
+        <div className="content">
+          <h4 className="header">{name}</h4>
+          <div className="description">{department}</div>
         </div>
-      </nav>
-    </header>
-  );
+      </li>
+    );
+  }
 }
 
-function ContactsList() {
-  return (
-    <ul className="ui relaxed divided list selection">
-      <ContactItem
-        login="typeofweb1"
-        name="Lena"
-        department="JavaScript Developer"
-      />
-      <ContactItem
-        login="typeofweb2"
-        name="Brian"
-        department="Human Resources"
-      />
-      <ContactItem
-        login="typeofweb3"
-        name="Rick"
-        department="QA"
-      />
-    </ul>
-  );
+class UserAvatar extends React.Component {
+  render() {
+    const { login } = this.props;
+    const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+    return (
+      <img src={imgUrl} className="ui mini rounded image" />
+    );
+  }
 }
 
-function ContactItem({ login, name, department }) {
-  return (
-    <li className="item">
-      <UserAvatar login={login}/>
-      <div className="content">
-        <h4 className="header">{name}</h4>
-        <div className="description">{department}</div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <AppHeader />
+        <main className="ui main text container">
+          <ContactsList />
+        </main>
       </div>
-    </li>
-  );
-}
-
-function UserAvatar({ login }) {
-  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
-  return (
-    <img src={imgUrl} className="ui mini rounded image" />
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <AppHeader />
-      <main className="ui main text container">
-        <ContactsList />
-      </main>
-    </div>
-  );
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
