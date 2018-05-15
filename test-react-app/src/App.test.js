92b4570b19d43531adb892bb1dddf720f43df7b5
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-05-16 00:52:57 +0200

    Add test - passes all users to the UserList

--- test-react-app/src/App.test.js
+++ test-react-app/src/App.test.js
@@ -16,3 +16,9 @@ it('includes UsersList', () => {
   const app = shallow(<App />);
   expect(app.containsMatchingElement(<UsersList />)).toEqual(true)
 });
+
+it(`passes all users to the UsersList`, () => {
+  const allUsers = ['old_user1', 'old_user2', 'old_user3', 'new_user1', 'new_user2'];
+  const app = shallow(<App />);
+  expect(app.find('UsersList').prop('users')).toEqual(allUsers);
+});
