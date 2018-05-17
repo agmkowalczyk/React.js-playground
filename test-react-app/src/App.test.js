e4388a1b4b3989afedad85d961adf76e506b2738
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-05-17 15:16:35 +0200

    Add test: filters names on input and empty input

--- test-react-app/src/App.test.js
+++ test-react-app/src/App.test.js
@@ -22,3 +22,21 @@ it(`passes all users to the UsersList`, () => {
   const app = shallow(<App />);
   expect(app.find('UsersList').prop('users')).toEqual(allUsers);
 });
+
+it('filters names on input', () => {
+  const allUsers = ['old_user1', 'old_user2', 'old_user3', 'new_user1', 'new_user2'];
+  const app = shallow(<App />);
+  expect(app.find('UsersList').prop('users')).toEqual(allUsers);
+
+  app.find('input').simulate('input', { target: { value: 'n' } })
+  expect(app.find('UsersList').prop('users')).toEqual(['new_user1', 'new_user2']);
+});
+
+it('filters names on empty input', () => {
+  const allUsers = ['old_user1', 'old_user2', 'old_user3', 'new_user1', 'new_user2'];
+  const app = shallow(<App />);
+  expect(app.find('UsersList').prop('users')).toEqual(allUsers);
+
+  app.find('input').simulate('input', { target: { value: '' } })
+  expect(app.find('UsersList').prop('users')).toEqual(allUsers);
+});
