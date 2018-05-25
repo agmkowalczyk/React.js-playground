7e8c12a6598f2ddf1ce80c8486cbc74c70831a4e
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-05-26 00:10:21 +0200

    FileInput component

---
+++ fileInput.js
@@ -0,0 +1,22 @@
+class FileInput extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.submitForm}>
+        <label>
+          <input
+            type="file"
+            ref={input => this.fileInput = input}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    console.log(this.fileInput.files[0].name);
+  }
+}
+
+ReactDOM.render(<FileInput />, document.getElementById('app'));
