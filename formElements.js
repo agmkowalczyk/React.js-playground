07268534f74d4296f26ce9951c8b354a5a7ae4bf
agmkowalczyk <agm.kowalczyk@gmail.com>
2018-05-25 23:49:28 +0200

    Change to uncontrolled components

--- formElements.js
+++ formElements.js
@@ -13,25 +13,33 @@ class App extends React.Component {
     this.setState({ [name]: value })
   }
 
+  submitForm = () => {
+    console.log(this.input.value);
+    console.log(this.select.value);
+    console.log(this.text.value);
+    console.log(this.checkbox.checked);
+  }
+
   render() {
     return(
       <div>
         <div>
-          <input name='name' value={this.state.name} onChange={this.handleInputChange} />
+          <input name='name' ref={input => this.input = input} defaultValue="Name..." onChange={this.handleInputChange} />
         </div>
         <div>
-          <select name='color' value={this.state.color} onChange={this.handleInputChange}>
+          <select name='color' ref={select => this.select = select} onChange={this.handleInputChange}>
             <option value='red'>Red</option>
             <option value='blue'>Blue</option>
             <option value='green'>Green</option>
           </select>
         </div>     
         <div>
-          <textarea name='message' value={this.state.value} onChange={this.handleInputChange}></textarea>
+          <textarea name='message' ref={text => this.text = text} defaultValue="Textarea..."  onChange={this.handleInputChange}></textarea>
         </div>  
         <div>
-          <input name='isChecked' type='checkbox' checked={this.state.isChecked} onChange={this.handleInputChange}/>
+          <input name='isChecked' type='checkbox' ref={checkbox => this.checkbox = checkbox} checked={this.state.isChecked} onChange={this.handleInputChange}/>
         </div>
+        <button onClick={this.submitForm}>Submit</button>
       </div>
     );
   }
